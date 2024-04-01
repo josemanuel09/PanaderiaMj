0dc826e0be0fce49bba1959557a9404c554cb999
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-03-31 20:09:24 -0400

    Agregando Mi Contexto

--- PanaderiaMj/PanaderiaMj/DAL/Contexto.cs
+++ PanaderiaMj/PanaderiaMj/DAL/Contexto.cs
@@ -5,6 +5,10 @@ namespace PanaderiaMj.DAL
 {
     public class Contexto : DbContext
     {
+        public DbSet<Compras> Compras { get; set; }
+        public DbSet<Insumos> Insumos { get; set; }
+        public DbSet<Proveedores> Proveedores { get; set; }
+        public DbSet<Empleados> Empleados { get; set; }
         public DbSet<Ventas> Ventas { get; set; }
         public DbSet<Productos> Productos { get; set; }
         public DbSet<Clientes> Clientes { get; set; }
