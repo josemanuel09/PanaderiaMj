8f4e3949b8917fc8825e28beb6b1044202ac5e03
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-31 20:00:45 -0400

    Creando modelo de ventas

---
+++ PanaderiaMj/PanaderiaMj/Models/Ventas.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PanaderiaMj.Models
+{
+    public class Ventas
+    {
+        [Key]
+        public int VentaId { get; set; }
+        [Required(ErrorMessage = "El Campo Fecha es obligatorio")]
+        public DateTime FechaVenta { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "El Campo Cliente es obligatorio")]
+        [ForeignKey("ClienteId")]
+        public int ClienteId { get; set; }
+        [Required(ErrorMessage = "El Campo Concepto es obligatorio")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Este campo solo acepta letras")]
+        public string? Concepto { get; set; }
+        [Required(ErrorMessage = "El Campo Tipo de Venta es obligatorio")]
+        [RegularExpression(@"^[a-zA-Z\s]*$", ErrorMessage = "Este campo solo acepta letras")]
+        public string? TipoVenta { get; set; }
+        [Required(ErrorMessage = "El Campo Sub Total es obligatorio")]
+        [Range(0, double.MaxValue, ErrorMessage = "El SubTotal no puede ser negativo")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Este campo solo acepta números")]
+        public double SubTotal { get; set; }
+        [Required(ErrorMessage = "El Campo Monto Total es obligatorio")]
+        [Range(0, double.MaxValue, ErrorMessage = "El Monto Total no puede ser negativo")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Este campo solo acepta números")]
+        public double MontoTotal { get; set; }
+
+        [ForeignKey("VentaId")]
+        public ICollection<VentasDetalle> DetalleVentas { get; set; } = new List<VentasDetalle>();
+    }
+}
