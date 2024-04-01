b7a6edff2d9b78cd19fa753204a410b4463a9c13
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-31 20:01:29 -0400

    Creando el modelo de VentasDetalle

---
+++ PanaderiaMj/PanaderiaMj/Models/VentasDetalle.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PanaderiaMj.Models
+{
+    public class VentasDetalle
+    {
+        [Key]
+        public int DetalleVentaId { get; set; }
+        [Required(ErrorMessage = "El Campo Producto es obligatorio")]
+        [ForeignKey("ProductoId")]
+        public int ProductoId { get; set; }
+        [Required(ErrorMessage = "El Campo Cantidad es obligatorio")]
+        [Range(1, 1000, ErrorMessage = "La cantidad debe ser mayor a 0")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Este campo solo acepta números")]
+        public int Cantidad { get; set; }
+        [Required(ErrorMessage = "El Campo Precio es obligatorio")]
+        [Range(0, double.MaxValue, ErrorMessage = "El Precio no puede ser negativo")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Este campo solo acepta números")]
+        public double Precio { get; set; }
+    }
+}
