ba52a1b72d68da7e2ceddb53d49b29e68d6f671e
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-03-31 20:08:37 -0400

    Agregando Compras y Su Detalle

---
+++ PanaderiaMj/PanaderiaMj/Models/Compras.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PanaderiaMj.Models
+{
+    public class Compras
+    {
+        [Key]
+        public int CompraId { get; set; }
+        [Required(ErrorMessage = "El Campo Proveedor es obligatorio")]
+        [ForeignKey("ProveedorId")]
+        public int ProveedorId { get; set; }
+        [Required(ErrorMessage = "El Campo Metodo de Pago es obligatorio")]
+
+        public string? MetodoDePago { get; set; }
+        [Required(ErrorMessage = "El campo Monto Total es obligatorio")]
+        [Range(0, double.MaxValue, ErrorMessage = "El Monto Total no puede ser negativo")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Este campo solo acepta números")]
+
+        public double MontoTotal { get; set; }
+        [Required(ErrorMessage = "El Campo Fecha es obligatorio")]
+        public DateTime FechaPedida { get; set; } = DateTime.Now;
+        public string? Observaciones { get; set; }
+
+        [ForeignKey("CompraId")]
+        public ICollection<ComprasDetalle> ComprasDetalle { get; set; } = new List<ComprasDetalle>();
+    }
+}
---
+++ PanaderiaMj/PanaderiaMj/Models/ComprasDetalle.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PanaderiaMj.Models
+{
+    public class ComprasDetalle
+    {
+        [Key]
+        public int CompraDetalleId { get; set; }
+        [ForeignKey("InsumoId")]
+        [Required(ErrorMessage = "El Campo Insumo es obligatorio")]
+        public int InsumoId { get; set; }
+        [ForeignKey("ProveedorId")]
+        [Required(ErrorMessage = "El Campo Proveedor es obligatorio")]
+        public int ProveedorId { get; set; }
+
+        [Required(ErrorMessage = "El Campo Cantidad es obligatorio")]
+        [Range(0, int.MaxValue, ErrorMessage = "La cantidad no puede ser negativa")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Este campo solo acepta números")]
+        public int Cantidad { get; set; }
+
+        [Required(ErrorMessage = "El Campo Precio es obligatorio")]
+        [Range(0, double.MaxValue, ErrorMessage = "El Precio no puede ser negativo")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Este campo solo acepta números")]
+        public double Precio { get; set; }
+    }
+}
