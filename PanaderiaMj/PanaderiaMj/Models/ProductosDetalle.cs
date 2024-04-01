978525315524d2199f9aa835c518fd3e8ab8c6b8
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-31 20:01:09 -0400

    Creando el modelo de Productos detalle

---
+++ PanaderiaMj/PanaderiaMj/Models/ProductosDetalle.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PanaderiaMj.Models
+{
+    public class ProductosDetalle
+    {
+        [Key]
+        public int ProductoDetalleId { get; set; }
+        [Required(ErrorMessage = "El Campo Insumo es Obligatorio")]
+        [ForeignKey("InsumoId")]
+        public int InsumoId { get; set; }
+
+        [Required(ErrorMessage = "El Campo Cantidad es Obligatorio")]
+        public int Cantidad { get; set; }
+    }
+}
