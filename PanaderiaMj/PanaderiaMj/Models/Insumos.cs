7015358c3c052c9ee94d60f0283acd48aae42ac7
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-03-31 20:05:21 -0400

    Creando Clase Insumos

---
+++ PanaderiaMj/PanaderiaMj/Models/Insumos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PanaderiaMj.Models
+{
+    public class Insumos
+    {
+        [Key]
+        public int InsumoId { get; set; }
+
+        [Required(ErrorMessage = "El Campo Nombre obligatorio")]
+        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "El Campo Cantidad obligatorio")]
+        public int CantidadDisponible { get; set; }
+    }
+}
