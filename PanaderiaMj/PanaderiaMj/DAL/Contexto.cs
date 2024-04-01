46592540fa62e4d4369c34fa073253eb4dbc50cb
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-03-31 20:01:45 -0400

    Creando el contexto

---
+++ PanaderiaMj/PanaderiaMj/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PanaderiaMj.Models;
+
+namespace PanaderiaMj.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Ventas> Ventas { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Recepciones> Recepciones { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        
+
+        
+    }
+    
+}
