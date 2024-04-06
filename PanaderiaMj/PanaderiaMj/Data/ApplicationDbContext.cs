61b618bdb3fe7f67f47e9012167db6cb3eba70d8
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-04-06 16:10:00 -0400

    Agregando Dbsets al nuevo contexto que utilizaremos.

--- PanaderiaMj/PanaderiaMj/Data/ApplicationDbContext.cs
+++ PanaderiaMj/PanaderiaMj/Data/ApplicationDbContext.cs
@@ -1,9 +1,18 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PanaderiaMj.Models;
 
 namespace PanaderiaMj.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Compras> Compras { get; set; }
+        public DbSet<Insumos> Insumos { get; set; }
+        public DbSet<Proveedores> Proveedores { get; set; }
+        public DbSet<Empleados> Empleados { get; set; }
+        public DbSet<Ventas> Ventas { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Recepciones> Recepciones { get; set; }
     }
 }
