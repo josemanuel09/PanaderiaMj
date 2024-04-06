f07605fcdd3365a291add8307bae8af41768d177
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-04-06 16:10:31 -0400

    Agregando campos al Application User.

--- PanaderiaMj/PanaderiaMj/Data/ApplicationUser.cs
+++ PanaderiaMj/PanaderiaMj/Data/ApplicationUser.cs
@@ -5,6 +5,9 @@ namespace PanaderiaMj.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
     }
 
 }
