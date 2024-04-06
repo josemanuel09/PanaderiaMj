00292f2aea846f01bc145f8e109a23f4965bbc07
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-04-06 16:14:17 -0400

    Cambiando contexto en la referencia de EmpleadosService.

--- PanaderiaMj/PanaderiaMj/Service/EmpleadosService.cs
+++ PanaderiaMj/PanaderiaMj/Service/EmpleadosService.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using PanaderiaMj.DAL;
+using PanaderiaMj.Data;
 using PanaderiaMj.Models;
 using System.Linq.Expressions;
 
@@ -7,8 +7,8 @@ namespace PanaderiaMj.Service
 {
     public class EmpleadosService
     {
-        private readonly Contexto _contexto;
-        public EmpleadosService(Contexto contexto)
+        private readonly ApplicationDbContext _contexto;
+        public EmpleadosService(ApplicationDbContext contexto)
         {
             _contexto = contexto;
         }
