f2ca656b90d8e59520668a9f02f571e4a07ccacc
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-04-06 16:27:31 -0400

    Agregando atributos a la informacion del usuario

--- PanaderiaMj/PanaderiaMj.Client/UserInfo.cs
+++ PanaderiaMj/PanaderiaMj.Client/UserInfo.cs
@@ -6,5 +6,9 @@ namespace PanaderiaMj.Client
     {
         public required string UserId { get; set; }
         public required string Email { get; set; }
+
+        public required string Role { get; set; }
+        public required string Nombre { get; set; }
+        public required string Apellidos { get; set; }
     }
 }
