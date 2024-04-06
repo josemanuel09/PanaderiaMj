f357b0ddc9050607827bb1a72cae39f9e1c9b6df
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-04-06 16:15:47 -0400

    Cambiando contexto en la referencia de VentasService.

--- PanaderiaMj/PanaderiaMj/Service/VentasService.cs
+++ PanaderiaMj/PanaderiaMj/Service/VentasService.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using PanaderiaMj.DAL;
+using PanaderiaMj.Data;
 using PanaderiaMj.Models;
 using System.Linq.Expressions;
 
@@ -7,8 +7,8 @@ namespace PanaderiaMj.Service
 {
     public class VentasService
     {
-        private readonly Contexto _contexto;
-        public VentasService(Contexto contexto)
+        private readonly ApplicationDbContext _contexto;
+        public VentasService(ApplicationDbContext contexto)
         {
             _contexto = contexto;
         }
